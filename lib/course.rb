059b944bb36bfacfea124fd34501c720addeffdb
Matt Parsons <parsonsmatt@gmail.com>
2015-02-05 11:57:48 -0500

    initialization tests and working

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,34 @@
 module Grooby
 	class Course
 		attr_accessor :major, :number
+
 		def initialize(*args)
+			defaults = {
+				major: "",
+				number: "",
+				prereqs: [],
+				offered: []
+			}
+
+			if args[0].is_a? String
+				@major = parse_major args[0]
+				@number = parse_number args[0]
+			elsif args[0].is_a? Hash
+				props = defaults.merge args[0]
+				@major = props[:major]
+				@number = props[:number]
+			end
+			
 			yield self if block_given?
 		end
+
+		private
+			def	parse_major string
+				string.split(' ').first
+			end
+
+			def parse_number string
+				string.split(' ').last
+			end
 	end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,32 +3,29 @@ require_relative '../lib/course.rb'
 RSpec.describe Grooby::Course do
 	describe "#initialize" do
 		it "can initialize with course string" do
+			course = nil
 			expect { course = Grooby::Course.new "CSCI 3030" }.to_not raise_error
-			expect(course.major).to be "CSCI"
-			expect(course.number).to be "3030"
+			expect(course.major).to eq "CSCI"
+			expect(course.number).to eq "3030"
 		end
 
 		it "can initialize with options hash only" do
+			course = nil
 			expect { course = Grooby::Course.new major: "CSCI", number: "3030" }.to_not raise_error
-			expect(course.major).to be "CSCI"
-			expect(course.number).to be "3030"
-		end
-
-		it "can initialize with string and options hash" do
-			expect { 
-				course = Grooby::Course.new "CSCI 3030", prereqs: [], offered: []
-			}.to_not raise_error
-			expect(course.major).to be "CSCI"
-			expect(course.number).to be "3030"
+			expect(course.major).to eq "CSCI"
+			expect(course.number).to eq "3030"
 		end
 
 		it "can initialize with block" do
+			course = nil
 			expect {
 				course = Grooby::Course.new do |c|
 					c.major = "CSCI"
 					c.number = "3030"
 				end
 			}.to_not raise_error
+			expect(course.major).to eq "CSCI"
+			expect(course.number).to eq "3030"
 		end
 	end
 end
