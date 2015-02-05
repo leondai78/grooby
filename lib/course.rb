1c5d7b324ed86affbbde461b41b2ae2f11b58db9
Matt Parsons <parsonsmatt@gmail.com>
2015-02-05 11:33:44 -0500

    Removed erroring on empty initializer

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,6 @@ module Grooby
 	class Course
 		attr_accessor :major, :number
 		def initialize(*args)
-			raise ArgumentError.new "Must initialize with values" if args == nil && !block_given?
 			yield self if block_given?
 		end
 	end
