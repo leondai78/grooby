df3426437b68b30698d046ff7e253d425f5c9990
Matt Parsons <parsonsmatt@gmail.com>
2015-03-26 15:58:33 -0400

    Update course.rb

--- lib/course.rb
+++ lib/course.rb
@@ -2,10 +2,10 @@ module Grooby
   class Course
     def initialize
       @major
-			@number
-			@offered = []
-			@prereqs = []
-			@required_for = []
-		end
-	end
+      @number
+      @offered = []
+      @prereqs = []
+      @required_for = []
+    end
+  end
 end
